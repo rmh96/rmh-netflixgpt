0111c165a0fea5591353ef6f8b94b51fcca92fb0
rmh96 <100729581+rmh96@users.noreply.github.com>
2024-04-03 19:08:41 +0530

    added changes in profileHOC

--- src/components/ProfileEditHOC.jsx
+++ src/components/ProfileEditHOC.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect } from "react";
 
 const ProfileEditHOC = (ProfileSectionComp) => {
+  const handleProfileEdit = () => {};
   return function withEditOption(props) {
     return (
       <div className="relative h-full w-full">
         <ProfileSectionComp {...props} />
-        <div className="absolute w-52 h-52 inset-0 bg-black bg-opacity-30 flex justify-center items-center text-5xl text-white">
+        <div
+          className="absolute w-52 h-52 inset-0 bg-black bg-opacity-30 flex justify-center items-center text-5xl text-white"
+          onClick={handleProfileEdit}
+        >
           ✎
         </div>
       </div>
